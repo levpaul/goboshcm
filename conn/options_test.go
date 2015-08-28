7d9926b75a5d9ca8ecf610353c15eac3ff866837
Levi Lovelock <llovelock@atlassian.com>
2015-08-28 11:14:37 -0500

    added first tests

---
+++ conn/options_test.go
@@ -0,0 +1,33 @@
+package conn
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	handler *mux.Router = ConstructRouter()
+	connUrl string      = "http://localhost:5280/http-bind"
+)
+
+func TestOptionsEndpointExistsReturns200(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	req, _ := http.NewRequest("OPTIONS", connUrl, nil)
+
+	handler.ServeHTTP(recorder, req)
+
+	assert.Equal(t, 200, recorder.Code)
+}
+
+func TestOptionsEndpointReturnsNoBody(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	req, _ := http.NewRequest("OPTIONS", connUrl, nil)
+
+	handler.ServeHTTP(recorder, req)
+
+	assert.Equal(t, "", recorder.Body.String())
+}
