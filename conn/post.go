94f84ce823f91bf5ad485fe4cbf8a9594b48f403
Levi Lovelock <lovelock.levi@gmail.com>
2015-08-28 21:08:49 -0700

    Added xml parsing of a valid first request

--- conn/post.go
+++ conn/post.go
@@ -1,15 +1,27 @@
 package conn
 
 import (
-	"encoding/xml"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
+
+	"encoding/xml"
 )
 
 type Payload struct {
-	Body string `xml:"rid,attr"`
+	XMLName   xml.Name `xml:"body"`
+	Rid       int      `xml:"rid,attr"`
+	To        string   `xml:"to,attr"`
+	Xmlns     string   `xml:"xmlns,attr"`
+	XmlLang   string   `xml:"xmllang,attr"`
+	Wait      int      `xml:"wait,attr"`
+	Hold      int      `xml:"hold,attr"`
+	Content   string   `xml:"content,attr"`
+	Ver       string   `xml:"ver,attr"`
+	XmppVer   string   `xml:"xmppversion,attr"`
+	XmlnsXmpp string   `xml:"xmlnsxmpp,attr"`
+	Route     string   `xml:"route,attr"`
 }
 
 // The real workhorse
@@ -17,15 +29,25 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 	getCommonHeaders(w, r)
 
 	body, readErr := ioutil.ReadAll(r.Body)
+
+	/* TODO: This is a hack because the encoding/xml library can't
+	*  handle colons in the attr names. See the following isse
+	*  https://github.com/golang/go/issues/11735 */
+	body = sanitizeXml(body)
+
 	if readErr != nil {
-		log.Println("Erorr reading body from request!")
+		log.Println("Error reading body from request!")
 	}
 	defer r.Body.Close()
 
 	var payload Payload
-	xml.Unmarshal(body, payload)
 
-	fmt.Println("P:", payload)
+	err := xml.Unmarshal(body, &payload)
+	if err != nil {
+		log.Println("Failed to unmarshal xml payload:", err.Error())
+		w.WriteHeader(400)
+		return
+	}
 
 	/*
 			   Overview
@@ -43,5 +65,13 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 		       - get innactivity
 
 	*/
+}
 
+// TODO: Remove this function or optimise it
+func sanitizeXml(b []byte) []byte {
+	s := string(b)
+	s = strings.Replace(s, "xml:lang=", "xmllang=", 1)
+	s = strings.Replace(s, "xmpp:version=", "xmllang=", 1)
+	s = strings.Replace(s, "xmlns:xmpp=", "xmlnsxmpp=", 1)
+	return []byte(s)
 }
--- conn/post_test.go
+++ conn/post_test.go
@@ -6,6 +6,8 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"encoding/xml"
+
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 )
@@ -13,15 +15,46 @@ import (
 var (
 	postTestHandler *mux.Router = ConstructRouter()
 	postTestConnUrl string      = "http://localhost:5280/http-bind"
+
+	validFirstPostRequestBody = []byte(`<body
+        rid='2902921866'
+        xmlns='http://jabber.org/protocol/httpbind'
+        to='chat.mysite.com'
+        xmllang='en'
+        wait='60'
+        hold='1'
+        content='text/xml; charset=utf-8'
+        ver='1.6'
+        xmppversion='1.0'
+        xmlnsxmpp='urn:xmpp:xbosh'
+        route='xmpp:mysite.com:5999'/>`)
 )
 
-func TestOptionsParseXml(t *testing.T) {
+func TestPostReturns200(t *testing.T) {
 	recorder := httptest.NewRecorder()
 
-	var xmlStr = []byte(`<body rid='123456' xmlns='http://jabber.org/protocol/httpbind' to='chat.hello.com' />`)
-	req, _ := http.NewRequest("POST", optionsTestConnUrl, bytes.NewBuffer(xmlStr))
+	req, _ := http.NewRequest("POST", optionsTestConnUrl, bytes.NewBuffer(validFirstPostRequestBody))
 
 	optionsTestHandler.ServeHTTP(recorder, req)
 
 	assert.Equal(t, 200, recorder.Code)
+}
+
+func TestParsingPayload(t *testing.T) {
+	var pl Payload
+	err := xml.Unmarshal(validFirstPostRequestBody, &pl)
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, 2902921866, pl.Rid)
+	assert.Equal(t, "http://jabber.org/protocol/httpbind", pl.Xmlns)
+	assert.Equal(t, "chat.mysite.com", pl.To)
+	assert.Equal(t, "en", pl.XmlLang)
+	assert.Equal(t, 60, pl.Wait)
+	assert.Equal(t, 1, pl.Hold)
+	assert.Equal(t, "text/xml; charset=utf-8", pl.Content)
+	assert.Equal(t, "1.6", pl.Ver)
+	assert.Equal(t, "1.0", pl.XmppVer)
+	assert.Equal(t, "urn:xmpp:xbosh", pl.XmlnsXmpp)
+	assert.Equal(t, "xmpp:mysite.com:5999", pl.Route)
 }
