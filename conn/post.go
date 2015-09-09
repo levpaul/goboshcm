c7ff764b480ab82efb88e99e7c8bb04a6f488944
Levi Lovelock <lovelock.levi@gmail.com>
2015-09-09 20:52:42 +1000

    Removed spurious comment

--- conn/post.go
+++ conn/post.go
@@ -64,21 +64,4 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(400)
 		return
 	}
-
-	/*
-			   Overview
-			    1. Parse the xml in the body (400 on bad parse) https://golang.org/pkg/encoding/xml/
-			    2. Check for sid -> if exists
-		          - get Session
-		          if can't find, return 400 else:
-
-			    3. Else create new session
-		       - check RID (error if not exist)
-		       - check to (error if not exist)
-		       - check white/blacklist TODO: ADD LATER
-		       - get wait (and set a default of 3)
-		       - get xml:lang - default to 'en'
-		       - get innactivity
-
-	*/
 }
